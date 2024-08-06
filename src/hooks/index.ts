2c21ad3af07ee75348a20907effe8bd721278d75
moonformeli <mgyang95@gmail.com>
2024-08-06 18:35:59 +0900

    refactor: changed to use .env

---
+++ src/hooks/index.ts
@@ -0,0 +1,2 @@
+export * from './useFirebaseInit';
+export * from './useGoogleLogin';
--- src/hooks/useFirebaseInit.ts
+++ src/hooks/useFirebaseInit.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { initializeApp, type FirebaseApp } from 'firebase/app';
 
-export const useFirebase = () => {
+export const useFirebaseInit = () => {
   const [fbApp, setFbApp] = useState<FirebaseApp | null>(null);
 
   useEffect(() => {
@@ -10,12 +10,12 @@ export const useFirebase = () => {
     // https://firebase.google.com/docs/web/setup#available-libraries
     if (!fbApp) {
       const firebaseConfig = {
-        apiKey: 'AIzaSyBWwUcNob9pIazHbhzDvAUskybAosKnZUU',
-        authDomain: 'elice-practice-31bde.firebaseapp.com',
-        projectId: 'elice-practice-31bde',
-        storageBucket: 'elice-practice-31bde.appspot.com',
-        messagingSenderId: '594914395919',
-        appId: '1:594914395919:web:94730774d25b78dabca07a',
+        apiKey: process.env.REACT_APP_API_KEY,
+        authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+        projectId: process.env.REACT_APP_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_APP_ID,
       };
 
       // Initialize Firebase
