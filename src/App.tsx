f91295a3dc5620212cb38d6d9216410cbbdf6c8c
moonformeli <mgyang95@gmail.com>
2024-08-08 18:15:32 +0900

    feat: google signin feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { useGoogleLogin } from './hooks';
+
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const { signIn } = useGoogleLogin();
+
+  const handleSignIn = async () => {
+    const credential = await signIn();
+
+    if (credential?.accessToken) {
+      setIsSignedIn(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div></div>
+      {isSignedIn ? (
+        <h1>로그인 완료</h1>
+      ) : (
+        <button onClick={handleSignIn}>구글 로그인</button>
+      )}
     </div>
   );
 }
---
+++ src/hooks/useGoogleLogin.tsx
@@ -0,0 +1,67 @@
+import {
+  getAuth,
+  signInWithPopup,
+  signOut as googleSignOut,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+export const useGoogleLogin = () => {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  console.log('앱 in useGoogleLogin', auth);
+
+  const signIn = () => {
+    if (!auth) {
+      console.error('auth is not defined');
+      return;
+    }
+
+    return signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        console.log(credential);
+        // const token = credential.accessToken;
+        // The signed-in user info.
+        // const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+
+        if (!credential) {
+          throw Error('credential is null');
+        }
+
+        return credential;
+      })
+      .catch((error) => {
+        console.log(error);
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+
+        return null;
+      });
+  };
+
+  const signOut = () => {
+    if (!auth) {
+      console.error('auth is not defined');
+      return;
+    }
+
+    googleSignOut(auth)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return { signIn, signOut };
+};
